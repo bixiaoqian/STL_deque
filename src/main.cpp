59d4dbd4b43f297d467a6ab655d3f5f8620c85c3
bixiaoqian <2461145450@qq.com>
2023-02-10 11:01:30 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// int x=9999;
+
+// int fun(int x){
+//     int count = 0;
+//     while (x ){
+//         count++;
+//         x=x&(x-1);
+//     }
+//     return count;
+// }
+
+int main()
+
+{
+    // int a=fun(x);
+
+    // cout<<a;
+
+	long i=32;
+	int a=3.1415926;
+	while(i--)
+	a*=2;
+
+	cout << a << endl;
+
+    system("pause");
+
+    return 0;
+}
